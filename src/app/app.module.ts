719fce0d3b37c6240242ccc48e0b21436afeab9a
Sonnerz <Sonnerz@users.noreply.github.com>
2020-09-13 00:17:47 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' }, // app loads it defaults to welcome
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' } // might be used for 404
+    ]),
+    ProductModule,
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
